8ec60c30cca09d6340f929841dc15f25e2f2ae83
shawntan <shawn@wtf.sg>
2015-06-20 15:01:12 +0800

    Stack implementation

--- stack.py
+++ stack.py
@@ -9,7 +9,7 @@ def cumsum(sequence):
     sumseq,_ = theano.scan(
             lambda x,csum: x+csum,
             sequences = [sequence],
-            outputs_info = [np.int32(0)]
+            outputs_info = [np.float32(0)]
         )
     return sumseq
 
@@ -18,14 +18,22 @@ def build(size):
     def init(sequence_length):
         initial_V = T.alloc(np.float32(0),(sequence_length,size))
         initial_s = T.alloc(np.float32(0),(sequence_length,))
-        def step(t,prev_V,prev_s,v,d,u):
-            V = T.concatenate([prev_V[:t],v,initial_V[t+1:]])
-            
+        def step(t,v,d,u,prev_V,prev_s):
+            prev_V = prev_V[:t]
+            V = T.concatenate([prev_V,v,initial_V[t+1:]])
 
+            rev_cum_prev_s = cumsum(prev_s[:t][::-1])[::-1]
+            to_flip_ = u - rev_cum_prev_s
+            to_flip = (to_flip_ > 0) * to_flip_
+            new_s_ = prev_s - to_flip
+            new_s = (new_s_ > 0) * new_s_
+            s = T.concatenate([new_s,d,initial_s[t+1:]])
 
+            rev_cum_s = cumsum(new_s[::-1])[::-1] + d
+            flip_score_ = 1 - rev_cum_s
+            flip_score = (flip_score_ > 0) * flip_score_
+            score = T.min([new_s,flip_score],axis=0)
 
+            r = T.dot(score,prev_V) + d * v
 
-
-
-if __name__ == "__main__":
-    print cumsum(np.arange(10,dtype=np.int32)).eval()
+            return V,s,r
